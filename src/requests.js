893376e3d7e585b82e526253b458b1c6e30b4057
Blnkfc <pavlinus11v@gmail.com>
2024-09-06 01:03:36 +0300

    Added styles for setting buttons of notes and confirm delete form.

--- src/requests.js
+++ src/requests.js
@@ -58,37 +58,103 @@ const rePopulateContent = async (fetchId) => {
       case 2:{
         const allNoteItems = await fetchAllNotes()
         console.log(`all notes ${allNoteItems}`)
-        const noteContentReadOnlyMode = 'text-lg pl-1 bg-transparent text-white border-none outline-none'
+        const noteContentReadOnlyMode = 'text-lg pl-1 bg-transparent text-white border-2 border-solid border-transparent outline-none'
         allNoteItems.forEach(n => {
+          //Creating details tag with fetched content inside
           const noteElement = document.createElement('details')
           const noteTitle = document.createElement('summary')
           const noteContent = document.createElement('input')
           const noteContentWrapper = document.createElement('div')
           noteContent.readOnly = true
 
+          //INITIATING Edit/Save/Delete Note
           const noteSettings = document.createElement('div')
           const settingsEdit = document.createElement('button')
+          const settingsEditImg = document.createElement('img')
+          settingsEditImg.src = 'https://i.imgur.com/c4ZYU6r.png'
+          settingsEdit.appendChild(settingsEditImg)
           const settingsSave = document.createElement('button')
+          const settingsSaveImg = document.createElement('img')
+          settingsSaveImg.src = 'https://i.imgur.com/5c7JrnW.png'
+          settingsSave.appendChild(settingsSaveImg)
           const settingsDelete = document.createElement('button')
+          const settingsDeleteImg = document.createElement('img')
+          settingsDeleteImg.src = 'https://i.imgur.com/Ey0yR4T.png'
+          settingsDelete.appendChild(settingsDeleteImg)
 
-          settingsEdit.textContent = "E"
-          settingsSave.textContent = "S"
-          settingsDelete.textContent = "D"
+          //CONFIRM DELETE
+          const confirmDelete = document.createElement('div')
+          const confirmDeleteText = document.createElement('span')
+          confirmDeleteText.textContent = 'Delete current note?'
+          const confirmDeleteBtn = document.createElement('button')
+          confirmDeleteBtn.textContent = 'CONFIRM'
+          addClass(confirmDelete, 'hidden scale-0 bg-red-400 justify-between ')
+          addClass(confirmDeleteText, 'text-lg ')
+          addClass(confirmDeleteBtn, 'text-lg font-bold border-solid border-2 border-red-600 rounded-lg ')
+
+          confirmDeleteBtn.addEventListener('click', () => {
+            removeClass(confirmDelete, 'scale-100')
+            addClass(confirmDelete, 'scale-0')
+            removeClass(noteSettings, 'hidden')
+            addClass(noteSettings, 'flex')
+          })
+
+          confirmDelete.appendChild(confirmDeleteText)
+          confirmDelete.appendChild(confirmDeleteBtn)
+
+
+
+          //SETTINGS BUTTONS STYLES
+          addClass(settingsEdit, ' w-10 h-10 p-[0.5em] border-solid border-4 border-slate-700 rounded-lg ')
+          addClass(settingsSave, ' w-10 h-10 hidden p-[0.5em] border-solid border-4 border-green-600 rounded-lg')
+          addClass(settingsDelete, ' w-10 h-10 p-[0.5em] ml-2 border-solid border-4 border-red-600 rounded-lg')
           noteSettings.appendChild(settingsEdit)
           noteSettings.appendChild(settingsSave)
           noteSettings.appendChild(settingsDelete)
 
+          //SETTINGS BUTTONS EVENT LISTENERS
+          //EDIT
+          settingsEdit.addEventListener('click', () => {
+            addClass(settingsEdit, 'hidden')
+            addClass(settingsSave, 'inline')
+            removeClass(settingsEdit, 'inline')
+            removeClass(settingsSave, 'hidden')
+            noteContent.readOnly = false
+            removeClass(noteContent, 'border-transparent')
+            addClass(noteContent, 'border-slate-700')
+            noteContent.focus()
+          })
+          //SAVE
+          settingsSave.addEventListener('click', () => {
+            addClass(settingsEdit, 'inline')
+            addClass(settingsSave, 'hidden')
+            removeClass(settingsEdit, 'hidden')
+            removeClass(settingsSave, 'inline')
+            noteContent.readOnly = true
+            removeClass(noteContent, 'border-slate-700')
+            addClass(noteContent, 'border-transparent')
+          })
+          //DELETE
+          settingsDelete.addEventListener('click', () => {
+            removeClass(confirmDelete, 'scale-0 ')
+            removeClass(confirmDelete, 'hidden')
+            addClass(confirmDelete, 'scale-100 flex')
+            removeClass(noteSettings, 'flex')
+            addClass(noteSettings, 'hidden')
+          })
           
 
           addClass(noteTitle, 'text-xl font-bold mx-1 pl-2')
           addClass(noteContent, noteContentReadOnlyMode)
-          addClass(noteContentWrapper, 'border-accent border-solid border-2 mx-1 bg-accent rounded-b-md scale-0 transition-all ease-linear group-open:scale-100 ')
+          addClass(noteSettings, ' block text-right mt-4')
+          addClass(noteContentWrapper, ' flex flex-col border-accent border-solid border-2 mx-1 bg-accent rounded-b-md scale-0 transition-all ease-linear group-open:scale-100 ')
           addClass(noteElement, 'open:border-solid open:border-2 open:border-secondary open:bg-accent hover:cursor-pointer group')
 
           noteTitle.textContent = n.title
           noteContent.value = n.content
           noteContentWrapper.appendChild(noteContent)
           noteContentWrapper.appendChild(noteSettings)
+          noteContentWrapper.appendChild(confirmDelete)
           noteElement.appendChild(noteTitle)
           noteElement.appendChild(noteContentWrapper)
           notePageContent.appendChild(noteElement)
