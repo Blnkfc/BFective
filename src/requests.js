91932990d0c5f6ce28ec0ea96fbd92ceaaf3e1ba
Blnkfc <pavlinus11v@gmail.com>
2024-09-18 19:17:49 +0300

    Added populating for reminders.

--- src/requests.js
+++ src/requests.js
@@ -6,13 +6,14 @@ function addClass(element, classStr) {
   if (element)
     element.className += ' ' + classStr;
 }
-
 class NoteConstructor {
   constructor(title, content) {
     this.title = title;
     this.content = content;
   }
 }
+
+
 
 
 const todoPageContent = document.getElementById('todo_content')
@@ -23,16 +24,22 @@ const fetchAllTodos = async () => {
   const data = await response.json()
   return data;
 }
+
+
 const fetchAllNotes = async () => {
   const response = await fetch('http://localhost:3000/api/notes')
   const data = await response.json()
   return data;
 }
+
+
 const fetchAllReminders = async () => {
   const response = await fetch('http://localhost:3000/api/reminders')
   const data = await response.json()
   return data;
 }
+
+
 
 const rePopulateContent = async (fetchId) => {
   todoPageContent.innerHTML = ""
@@ -63,10 +70,11 @@ const rePopulateContent = async (fetchId) => {
       break
     }
     case 2: {
-      const allNoteItems = await fetchAllNotes()
-      console.log(`all notes ${allNoteItems}`)
-      const noteContentReadOnlyMode = 'text-lg pl-1 bg-transparent text-white border-2 border-solid border-transparent outline-none'
-      allNoteItems.forEach(n => {
+      
+    const allNoteItems = await fetchAllNotes()
+    console.log(`all notes ${allNoteItems}`)
+    const noteContentReadOnlyMode = 'text-lg pl-1 bg-transparent text-white border-2 border-solid border-transparent outline-none'
+    allNoteItems.forEach(n => {
         //Creating details tag with fetched content inside
         const noteElement = document.createElement('details')
         const noteTitle = document.createElement('summary')
@@ -103,16 +111,16 @@ const rePopulateContent = async (fetchId) => {
         addClass(cancelDeleteBtn, 'text-lg color-slate-600 border-solid border-2 border-slate-600 rounded-lg px-2')
 
         confirmDeleteBtn.addEventListener('click', () => {
-          removeClass(confirmDelete, 'scale-100')
-          addClass(confirmDelete, 'scale-0 hidden')
-          removeClass(noteSettings, 'hidden')
-          addClass(noteSettings, 'block')
+            removeClass(confirmDelete, 'scale-100')
+            addClass(confirmDelete, 'scale-0 hidden')
+            removeClass(noteSettings, 'hidden')
+            addClass(noteSettings, 'block')
         })
         cancelDeleteBtn.addEventListener('click', () => {
-          removeClass(confirmDelete, 'scale-100')
-          addClass(confirmDelete, 'scale-0 hidden')
-          removeClass(noteSettings, 'hidden')
-          addClass(noteSettings, 'block')
+            removeClass(confirmDelete, 'scale-100')
+            addClass(confirmDelete, 'scale-0 hidden')
+            removeClass(noteSettings, 'hidden')
+            addClass(noteSettings, 'block')
         })
 
         confirmDelete.appendChild(confirmDeleteText)
@@ -132,55 +140,55 @@ const rePopulateContent = async (fetchId) => {
         //SETTINGS BUTTONS EVENT LISTENERS
         //EDIT
         settingsEdit.addEventListener('click', () => {
-          addClass(settingsEdit, 'hidden')
-          addClass(settingsSave, 'inline')
-          removeClass(settingsEdit, 'inline')
-          removeClass(settingsSave, 'hidden')
-          noteContent.readOnly = false
-          removeClass(noteContent, 'border-transparent')
-          addClass(noteContent, 'border-slate-700')
-          noteContent.focus()
+            addClass(settingsEdit, 'hidden')
+            addClass(settingsSave, 'inline')
+            removeClass(settingsEdit, 'inline')
+            removeClass(settingsSave, 'hidden')
+            noteContent.readOnly = false
+            removeClass(noteContent, 'border-transparent')
+            addClass(noteContent, 'border-slate-700')
+            noteContent.focus()
         })
 
 
         //SAVE
         settingsSave.addEventListener('click', () => {
-          console.log('initial save press')
-          addClass(settingsEdit, 'inline')
-          addClass(settingsSave, 'hidden')
-          removeClass(settingsEdit, 'hidden')
-          removeClass(settingsSave, 'inline')
-          noteContent.readOnly = true
-          removeClass(noteContent, 'border-slate-700')
-          addClass(noteContent, 'border-transparent')
+            console.log('initial save press')
+            addClass(settingsEdit, 'inline')
+            addClass(settingsSave, 'hidden')
+            removeClass(settingsEdit, 'hidden')
+            removeClass(settingsSave, 'inline')
+            noteContent.readOnly = true
+            removeClass(noteContent, 'border-slate-700')
+            addClass(noteContent, 'border-transparent')
 
-          const updateNote = async() => {
-            const body = new NoteConstructor(noteTitle.textContent, noteContent.value)
-            console.log(body)
-            try {
-              const response = await fetch(`http://localhost:3000/api/notes/${n?._id}`, {
-                method: "PUT",
-                headers: {
-                  "Content-Type": "application/json",
-                },
-                body: JSON.stringify(body)
-              })
-              const data = await response.json()
-              console.log(data)
-            } catch (error) {
-              console.log(`Error while updating requested note: ${error}`)
+            const updateNote = async () => {
+                const body = new NoteConstructor(noteTitle.textContent, noteContent.value)
+                console.log(body)
+                try {
+                    const response = await fetch(`http://localhost:3000/api/notes/${n?._id}`, {
+                        method: "PUT",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify(body)
+                    })
+                    const data = await response.json()
+                    console.log(data)
+                } catch (error) {
+                    console.log(`Error while updating requested note: ${error}`)
+                }
             }
-          }
-          updateNote()
+            updateNote()
 
         })
         //DELETE
         settingsDelete.addEventListener('click', () => {
-          removeClass(confirmDelete, 'scale-0 ')
-          removeClass(confirmDelete, 'hidden')
-          addClass(confirmDelete, 'scale-100 flex')
-          removeClass(noteSettings, 'flex')
-          addClass(noteSettings, 'hidden')
+            removeClass(confirmDelete, 'scale-0 ')
+            removeClass(confirmDelete, 'hidden')
+            addClass(confirmDelete, 'scale-100 flex')
+            removeClass(noteSettings, 'flex')
+            addClass(noteSettings, 'hidden')
         })
 
 
@@ -198,11 +206,34 @@ const rePopulateContent = async (fetchId) => {
         noteElement.appendChild(noteTitle)
         noteElement.appendChild(noteContentWrapper)
         notePageContent.appendChild(noteElement)
-      })
+    })
       break
     }
     case 3: {
-      allContentItems = await fetchAllReminders()
+      const allReminderItems = await fetchAllReminders()
+
+      allReminderItems.forEach(r => {
+        const reminderElement = document.createElement('details')
+        const reminderTitle = document.createElement('summary')
+        const reminderContent = document.createElement('div')
+        const reminderDate = document.createElement('div')
+        const reminderDateString = new Date(r.dateToRemind)
+
+
+        addClass(reminderTitle, 'text-xl font-bold mx-1')
+        addClass(reminderContent, 'text-white text-lg border-accent border-solid border-2 mx-1 bg-accent rounded-b-md scale-0 transition-all ease-linear group-open:scale-100')
+        addClass(reminderElement, 'open:border-solid open:border-2 open:border-secondary open:bg-accent hover:cursor-pointer group ')
+
+
+        reminderTitle.textContent = r.title
+        reminderContent.textContent = r.content
+        reminderDate.textContent = reminderDateString.toDateString()
+        reminderElement.appendChild(reminderTitle)
+        reminderElement.appendChild(reminderContent)
+        reminderElement.appendChild(reminderDate)
+        reminderPageContent.appendChild(reminderElement)
+      });
+      
       break
     }
   }
